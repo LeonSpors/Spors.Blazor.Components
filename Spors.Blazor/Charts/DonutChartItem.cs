7408f3eade4642a8dd33623b6192995b22cb5117
Leon Spors <15744333+LeonSpors@users.noreply.github.com>
2020-10-26 09:10:49 +0100

    Add DonutChart

---
+++ Spors.Blazor/Charts/DonutChartItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spors.Blazor.Charts
+{
+    public class DonutChartItem
+    {
+        public string Name { get; set; }
+
+        public int Percentage { get; set; }
+
+        public Color Fill { get; set; }
+    }
+}
---
+++ Spors.Blazor/ComponentInterop.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spors.Blazor
+{
+    class ComponentInterop : IAsyncDisposable
+    {
+        private readonly Lazy<Task<IJSObjectReference>> moduleTask;
+
+        public ComponentInterop(IJSRuntime jsRuntime)
+        {
+            moduleTask = new(() => jsRuntime.InvokeAsync<IJSObjectReference>(
+               "import", "./_content/spors.blazor/spors-blazor-components.js").AsTask());
+        }
+
+        public async ValueTask<bool> IsConicGradientSupported()
+        {
+            var module = await moduleTask.Value;
+            return await module.InvokeAsync<bool>("isConicGradientSupported");
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            if (moduleTask.IsValueCreated)
+            {
+                var module = await moduleTask.Value;
+                await module.DisposeAsync();
+            }
+        }
+    }
+}
