01280c5be01a025fb7f4886bb6df978d54521e5b
Leon Spors <15744335+halfbax@users.noreply.github.com>
2020-10-31 00:17:40 +0100

    Add javascript interop class

---
+++ Spors.Blazor/Interop.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.JSInterop;
+using System;
+using System.Threading.Tasks;
+
+namespace Spors.Blazor
+{
+    public class Interop : IAsyncDisposable
+    {
+        private readonly Lazy<Task<IJSObjectReference>> moduleTask;
+
+        public Interop(IJSRuntime runtime)
+        {
+            moduleTask = new(() => runtime.InvokeAsync<IJSObjectReference>(
+               "import", "./_content/Spors.Blazor/spors-blazor.js").AsTask());
+        }
+
+        public async ValueTask<bool> IsSupported(string property)
+        {
+            var module = await moduleTask.Value;
+            return await module.InvokeAsync<bool>("supportsProperty", property);
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            if (moduleTask.IsValueCreated)
+            {
+                var module = await moduleTask.Value;
+                await module.DisposeAsync();
+            }
+        }
+    }
+}
